d433f07d393a0ffc9d3a39fc7ea62e533c7d0e8d
Anurag Mondal <anorak318@gmail.com>
2020-11-20 14:45:39 +0530

    Add the code

---
+++ dup-rem.py
@@ -0,0 +1,18 @@
+import sys
+from time import sleep
+
+try: file1, file2 = sys.argv[1], sys.argv[2]
+except IndexError: sys.exit(print("Usage: python dup-rem.py input-file output-file"))
+
+set1 = set()
+count = 0
+print("\nRemoving duplicates........")
+
+with open (file1, 'r') as file1, open (file2, 'w') as file2:
+	for line in file1:
+		count += 1
+		set1.add(line)
+	for line in set1: file2.write(line)
+sleep(1)
+print ("\nTotal number of lines in the input file:", count)
+print ("Total number of lines after removing duplicates:", len(set1), end="\n\n")
